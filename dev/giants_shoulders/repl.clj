40edc341b008e920d363500bb94bde0685c0bb7b
Oliver Caldwell <ollie@oli.me.uk>
2025-05-02 12:09:16 +0100

    Strip the repl code back now that we have less deps

--- dev/giants_shoulders/repl.clj
+++ dev/giants_shoulders/repl.clj
@@ -1,36 +1,21 @@
 (ns giants-shoulders.repl
   (:require [nrepl.server :as nrepl]
             [cider.nrepl :as cider]
-            [malli.core :as m]
             [malli.dev :as malli-dev]
             [malli.dev.pretty :as malli-pretty]
             [taoensso.timbre :as log]
             [portal.api :as portal]
-            [mount.core :as mount]
             [rebel-readline.core :as rr]
             [rebel-readline.clojure.line-reader :as rr-clr]
             [rebel-readline.clojure.service.local :as rr-csl]
             [rebel-readline.clojure.main :as rr-cm]
             [clojure.main :as clj-main]))
 
-(mount/defstate system-status-logger
-  :start (log/info "Mount system started")
-  :stop (log/info "Mount system stopped"))
-
-(defn- add
-  "If you have LSP configured correctly you should see a type error / warning if you try to type (add :foo 10) inside this buffer."
-  [a b]
-  (+ a b))
-(m/=> add [:=> [:cat number? number?] number?])
-
 (defn start!
   "Start a development REPL, intended to be invoked from ./scripts/repl"
   [{:keys [portal]}]
   (log/info "Starting malli dev instrumentation")
   (malli-dev/start! {:report (malli-pretty/thrower)})
-
-  (log/info "Starting mount system")
-  (mount/start)
 
   (log/info "Starting nREPL server")
   (let [{:keys [port] :as _server} (nrepl/start-server :handler cider/cider-nrepl-handler)]
@@ -55,9 +40,6 @@
   (when portal
     (log/info "Closing portal")
     (portal/close))
-
-  (log/info "Stopping mount system")
-  (mount/stop)
 
   (shutdown-agents)
   (System/exit 0))
