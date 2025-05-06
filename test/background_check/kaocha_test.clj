e8518e5893a34fb758635e9dabc9c5d3d1cc2927
Oliver Caldwell <ollie@oli.me.uk>
2025-05-06 22:29:43 +0100

    Add a quick test of the kaocha plugin

--- test/background_check/kaocha_test.clj
+++ test/background_check/kaocha_test.clj
@@ -1,6 +1,13 @@
 (ns background-check.kaocha-test
-  (:require [clojure.test :as t]))
+  (:require [clojure.test :as t]
+            [spy.core :as spy]
+            [background-check.kaocha :as kaocha]
+            [background-check.runner :as runner]))
 
-;; TODO Test the Kaocha plugin hook, just make sure it calls the right function.
-(t/deftest hello-world
-  (t/is (= 1 1)))
+(t/deftest plugin-post-run-hook
+  (t/testing "it calls check-dirs with the dirs config"
+    (with-redefs [runner/check-dirs (spy/spy)]
+      (let [dirs ["foo"]]
+        (kaocha/plugin-post-run-hook {:background-check/dirs dirs})
+        (t/is (spy/called-once? runner/check-dirs))
+        (t/is (spy/called-with? runner/check-dirs dirs))))))
