4ed56017d3429f99ce96facf6211ff78a22b9cc0
Oliver Caldwell <ollie@oli.me.uk>
2025-05-06 22:47:22 +0100

    Starting to type check the kaocha plugin ns although failing at the moment

--- src/background_check/kaocha.clj
+++ src/background_check/kaocha.clj
@@ -1,10 +1,18 @@
 (ns background-check.kaocha
   "Plugin that executes Typed Clojure."
   (:require [kaocha.plugin :as kp]
-            [background-check.runner :as runner]))
+            [background-check.runner :as runner]
+            [typed.clojure :as t]))
+
+(t/ann
+ plugin-post-run-hook
+ [(t/HMap
+   :optional
+   {:background-check/dirs (t/Seqable t/Str)})])
 
 (kp/defplugin background-check.kaocha/plugin
   (post-run
    [result]
+   (assert (map? result))
    (runner/check-dirs (get result :background-check/dirs))
    result))
