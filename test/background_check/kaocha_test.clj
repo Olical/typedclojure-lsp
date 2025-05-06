47ec7536c90ffa046ea3f05370b2f967d1659f82
Oliver Caldwell <ollie@oli.me.uk>
2025-05-06 22:30:19 +0100

    Check that it returns the original options

--- test/background_check/kaocha_test.clj
+++ test/background_check/kaocha_test.clj
@@ -7,7 +7,8 @@
 (t/deftest plugin-post-run-hook
   (t/testing "it calls check-dirs with the dirs config"
     (with-redefs [runner/check-dirs (spy/spy)]
-      (let [dirs ["foo"]]
-        (kaocha/plugin-post-run-hook {:background-check/dirs dirs})
+      (let [dirs ["foo"]
+            opts {:background-check/dirs dirs}]
+        (t/is (= opts (kaocha/plugin-post-run-hook opts)))
         (t/is (spy/called-once? runner/check-dirs))
         (t/is (spy/called-with? runner/check-dirs dirs))))))
