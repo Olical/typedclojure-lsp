6fce868ac3e8f9304060e3a1213bf0fb1d6bc1d9
Oliver Caldwell <ollie@oli.me.uk>
2025-06-07 15:49:22 +0100

    Convert giants shoulders repl to telemere

--- dev/giants_shoulders/repl.clj
+++ dev/giants_shoulders/repl.clj
@@ -1,7 +1,7 @@
 (ns giants-shoulders.repl
   (:require [nrepl.server :as nrepl]
             [cider.nrepl :as cider]
-            [taoensso.timbre :as log]
+            [taoensso.telemere :as te]
             [portal.api :as portal]
             [rebel-readline.core :as rr]
             [rebel-readline.clojure.line-reader :as rr-clr]
@@ -13,28 +13,28 @@
   "Start a development REPL, intended to be invoked from ./scripts/repl"
   [{:keys [portal]}]
 
-  (log/info "Starting nREPL server")
+  (te/log! :info "Starting nREPL server")
   (let [{:keys [port] :as _server} (nrepl/start-server :handler cider/cider-nrepl-handler)]
-    (log/info "nREPL server started on port" port)
-    (log/info "Writing port to .nrepl-port")
+    (te/log! :info (str "nREPL server started on port " port))
+    (te/log! :info "Writing port to .nrepl-port")
     (spit ".nrepl-port" port))
 
   (when portal
-    (log/info "Opening portal, use (tap> ...) to inspect values")
+    (te/log! :info "Opening portal, use (tap> ...) to inspect values")
     (portal/open)
     (add-tap #'portal/submit))
 
-  (log/info "Starting interactive REPL")
+  (te/log! :info "Starting interactive REPL")
   (rr/with-line-reader
     (rr-clr/create (rr-csl/create))
     (clj-main/repl
      :prompt (fn [])
      :read (rr-cm/create-repl-read)))
 
-  (log/info "Shutting down")
+  (te/log! :info "Shutting down")
 
   (when portal
-    (log/info "Closing portal")
+    (te/log! :info "Closing portal")
     (portal/close))
 
   (shutdown-agents)
