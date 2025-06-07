caf9b5f554cbfa8ab85d188c127abf1172d701a1
Oliver Caldwell <ollie@oli.me.uk>
2025-06-07 14:02:50 +0100

    Snapshot with better logging to files before I move to stdio

--- src/typedclojure_lsp/lsp.clj
+++ src/typedclojure_lsp/lsp.clj
@@ -1,14 +1,14 @@
 (ns typedclojure-lsp.lsp
-  (:require [clojure.set :as set]
-            [clojure.core.async :as async]
-            [lsp4clj.io-server :as io-server]
+  (:require [clojure.java.io :as io]
+            [clojure.core.async :as a]
             [lsp4clj.server :as server]
+            [lsp4clj.io-server :as io-server]
             [aleph.tcp :as tcp]
             [aleph.netty :as netty]
             [manifold.stream :as s]
+            [clj-commons.byte-streams :as bs]
             [taoensso.telemere :as te]
-            [typedclojure-lsp.runner :as runner])
-  (:import [java.io OutputStream PipedInputStream PipedOutputStream]))
+            [typedclojure-lsp.runner :as runner]))
 
 ;; TODO Hook up https://github.com/nextjournal/beholder and typedclojure to type check on file change, or should it be driven by LSP?
 ;; TODO Hook the server up to a channel pair for testing.
@@ -16,12 +16,22 @@
 ;; TODO On textDocument/didOpen or textDocument/didSave, run the diagnostics. (almost there, Neovim isn't activating it though, so it's not attached to my buffer)
 ;; TODO Run textDocument/publishDiagnostics with the results.
 
-;; TODO Can this be cleaned up or simplified somehow? Am I missing a function that already exists that does this for me.
-;; TODO Can I just start a simpler socket server? I don't need manifold here https://github.com/clojure-lsp/lsp4clj?tab=readme-ov-file#other-types-of-servers
-(defn manifold-duplex->io-streams [duplex-stream]
-  (let [pout (PipedOutputStream.)
-        pin  (PipedInputStream. pout)
-        out (proxy [OutputStream] []
+(te/add-handler! :typedclojure-lsp/file (te/handler:file {:path ".typedclojure-lsp/logs/typedclojure-lsp.log"}))
+(te/remove-handler! :default/console)
+
+(Thread/setDefaultUncaughtExceptionHandler
+ (reify Thread$UncaughtExceptionHandler
+   (uncaughtException [_ thread ex]
+     (te/log!
+      {:level :error
+       :data {:thread (.getName thread)
+              :exception ex}}
+      "Uncaught exception in thread"))))
+
+(defn manifold-stream->io-pair [duplex-stream]
+  (let [pout (java.io.PipedOutputStream.)
+        pin (java.io.PipedInputStream. pout)
+        out (proxy [java.io.OutputStream] []
               (write
                 ([b]
                  (s/put! duplex-stream b))
@@ -31,61 +41,80 @@
 
     (s/consume
      (fn [msg]
-       (let [bytes (if (instance? (Class/forName "[B") msg)
-                     msg
-                     (.getBytes (str msg) "UTF-8"))]
-         (.write pout bytes)
-         (.flush pout)))
+       (.write pout msg)
+       (.flush pout))
      duplex-stream)
 
-    {:input-stream pin
-     :output-stream out}))
+    {:in pin
+     :out out}))
 
 ; (t/ann handler [manifold.stream.default.Stream map? :-> t/Nothing])
 (defn handler [duplex-stream client-info]
-  (te/log! {:level :info :data client-info} "New connection.")
-  (let [lsp-server
-        (io-server/server
-         (merge
-          {:trace-level "verbose"}
-          (set/rename-keys
-           (manifold-duplex->io-streams duplex-stream)
-           {:input-stream :in
-            :output-stream :out})))]
+  (try
+    (te/log! {:level :info :data client-info} "New connection.")
+    (let [lsp-server
+          (io-server/server
+           (merge
+            {:trace-level "verbose"}
+            (manifold-stream->io-pair duplex-stream)))]
 
-    (async/go-loop []
-      (when-let [[level & args] (async/<! (:trace-ch lsp-server))]
-        (te/log!
-         {:level :info
-          :data {:level level
-                 :args args}}
-         "lsp4clj")
-        (recur)))
+      (a/go-loop []
+        (when-let [[level & args] (a/<! (:log-ch lsp-server))]
+          (te/log!
+           {:level :info
+            :data {:level level
+                   :args args}}
+           "lsp4clj [log]")
+          (recur)))
 
-    (server/send-notification
-     lsp-server "window/showMessage"
-     {:message "hello from typedclojure-lsp"
-      :type "info"}))
-  nil)
+      (a/go-loop []
+        (when-let [[level & args] (a/<! (:trace-ch lsp-server))]
+          (te/log!
+           {:level :info
+            :data {:level level
+                   :args args}}
+           "lsp4clj [trace]")
+          (recur)))
+
+      (server/send-notification
+       lsp-server "window/showMessage"
+       {:message "hello from typedclojure-lsp\n"
+        :type "info"}))
+    nil
+    (catch Exception e
+      (te/log!
+       {:level :error
+        :data {:exception e}}
+       "Error in LSP handler")
+      (throw e))))
+
+(defmethod server/receive-request "initialize"
+  [_ context params]
+  (te/log!
+   {:level :info
+    :data {:context context
+           :params params}}
+   "initialize")
+  {})
 
 (defn type-check-and-notify! []
   (runner/check-dirs ["."]))
 
 (defmethod server/receive-notification "textDocument/didOpen"
-  [_ context opts]
+  [_ context params]
   (te/log!
    {:level :info
     :data {:context context
-           :opts opts}}
+           :params params}}
    "textDocument/didOpen")
   (type-check-and-notify!))
 
 (defmethod server/receive-notification "textDocument/didSave"
-  [_ context opts]
+  [_ context params]
   (te/log!
    {:level :info
     :data {:context context
-           :opts opts}}
+           :params params}}
    "textDocument/didSave")
   (type-check-and-notify!))
 
