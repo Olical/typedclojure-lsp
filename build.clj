8a65431b8e108d939859afab9147c72b55ac0876
Oliver Caldwell <ollie@oli.me.uk>
2025-06-14 13:11:50 +0100

    Got build and deploy of jar working

--- build.clj
+++ build.clj
@@ -1,14 +1,21 @@
 (ns build
-  (:require [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]
+            [deps-deploy.deps-deploy :as dd]))
 
-(def lib 'olical/typedclojure-lsp)
+(def lib 'uk.me.oli/typedclojure-lsp)
 
 ;; TODO Use the GitHub rev / tag
 ;; https://github.com/jlesquembre/clojars-publish-action/blob/89a4eb7bdbe1270621e6643250afce152701699e/src/entrypoint.clj#L41-L47
-(def version "0.0.2") ; (b/git-count-revs nil)
+(def version "0.0.1") ; (b/git-count-revs nil)
 
 (def class-dir "target/classes")
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(def pom-template
+  [[:licenses
+    [:license
+     [:name "UNLICENSE"]
+     [:url "https://unlicense.org/"]]]])
 
 (def basis (delay (b/create-basis {:project "deps.edn"})))
 
@@ -20,6 +27,7 @@
                 :lib lib
                 :version version
                 :basis @basis
+                :pom-data pom-template
                 :src-dirs ["src"]})
 
   (b/copy-dir {:src-dirs ["src" "resources"]
@@ -33,3 +41,10 @@
 
   (b/jar {:class-dir class-dir
           :jar-file jar-file}))
+
+(defn deploy "Deploy the JAR to Clojars." [opts]
+  (dd/deploy {:installer :remote :artifact (b/resolve-path jar-file)
+              :pom-file (b/pom-path
+                         {:lib lib
+                          :class-dir class-dir})})
+  opts)
